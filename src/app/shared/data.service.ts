8a0e8d4d98a0be64ffeb359bb78c8fb187587c4b
roberta-fiume <robertaf898@gmail.com>
2021-06-29 16:48:48 +0100

    added dynamic list

---
+++ src/app/shared/data.service.ts
@@ -0,0 +1,37 @@
+/* eslint-disable */
+
+import { Injectable } from '@angular/core';
+import { Todo } from './todo.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  todos: Todo[] = [
+    new Todo('this is a test!'),
+    new Todo('Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe odio sunt perspiciatis consectetur molestiae reiciendis exercitationem consequatur quisquam alias ipsam velit, quidem assumenda id explicabo temporibus. Culpa laudantium quo tempore')
+  ]
+
+  constructor() { 
+
+  }
+
+  getAllTodos() {
+    return this.todos;
+  }
+
+  addTodo(todo: Todo) {
+    this.todos.push(todo);
+  }
+
+  updateTodo(index: number, updateTodo: Todo) {
+    this.todos[index] = updateTodo;
+  }
+
+  deleteTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
+}
+
+
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../shared/data.service';
+import { Todo } from '../shared/todo.model';
 
 @Component({
   selector: 'app-todos',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
-  constructor() { }
+  todos: Todo[] = [];
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.todos = this.dataService.getAllTodos()
+
   }
 
 }
